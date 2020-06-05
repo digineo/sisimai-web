0fb0b97be898cdf122c6c333532d9a02cedc581e
Dominik Menke <dom@digineo.de>
2020-06-05 10:31:51 +0200

    fix intendation

--- spec/features/classifier_spec.rb
+++ spec/features/classifier_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe "mail classification" do
   UNKNOWN = -1
 
   {
-		"auto_earthlink"   => { bounce: UNKNOWN, reason: "vacation",     replycode: "" },
-		"bounce_aol"       => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_att"       => { bounce: SOFT,    reason: "spamdetected", replycode: "521" },
-		"bounce_charter"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_cox"       => { bounce: SOFT,    reason: "suspend",      replycode: "550" },
-		"bounce_earthlink" => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_exchange"  => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_gmail"     => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_hotmail"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_me"        => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_postfix"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
-		"bounce_spam"      => { bounce: SOFT,    reason: "spamdetected", replycode: "554" },
-		"bounce_yahoo"     => { bounce: SOFT,    reason: "filtered",     replycode: "554" },
-		"ordinary_gmail"   => { bounce: false },
+    "auto_earthlink"   => { bounce: UNKNOWN, reason: "vacation",     replycode: "" },
+    "bounce_aol"       => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_att"       => { bounce: SOFT,    reason: "spamdetected", replycode: "521" },
+    "bounce_charter"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_cox"       => { bounce: SOFT,    reason: "suspend",      replycode: "550" },
+    "bounce_earthlink" => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_exchange"  => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_gmail"     => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_hotmail"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_me"        => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_postfix"   => { bounce: HARD,    reason: "userunknown",  replycode: "550" },
+    "bounce_spam"      => { bounce: SOFT,    reason: "spamdetected", replycode: "554" },
+    "bounce_yahoo"     => { bounce: SOFT,    reason: "filtered",     replycode: "554" },
+    "ordinary_gmail"   => { bounce: false },
   }.each do |fixture, options|
     it "classifies #{fixture}" do
       message = read_fixture("messages/#{fixture}.txt")
